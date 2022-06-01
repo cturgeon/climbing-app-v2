4e68b58f1498a790ce5bc08cdb1b8ba00064278b
cturgeon <casey.turgeon@gmail.com>
2022-06-01 09:52:52 -0500

    change: home gym component, smaller first load

--- pages/user/settings.js
+++ pages/user/settings.js
@@ -1,58 +1,17 @@
 import { useSession } from "next-auth/react";
-import { useEffect, useState } from "react";
 
-import { Button, Select } from "@mantine/core";
+import { Button } from "@mantine/core";
 
+import dynamic from "next/dynamic";
+const DynamicComponent = dynamic(() =>
+  import("../../components/settings/home-gym")
+);
 export default function UserSettingsPage() {
   const { data: session } = useSession();
-  const [gym, setGym] = useState();
-  const [gymData, setGymData] = useState();
-
-  function submitHandler(event) {
-    event.preventDefault();
-    fetch("/api/homeGym", {
-      method: "POST",
-      body: JSON.stringify(gym),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-    setGym(null);
-  }
-
-  useEffect(() => {
-    fetch("/api/gymData")
-      .then((res) => res.json())
-      .then((data) => setGymData(data.gymData));
-  }, []);
-
-  if (!gymData) {
-    return null;
-  }
-
-  const selectData = [];
-  gymData.forEach((gym) =>
-    selectData.push({ value: `${gym._id}`, label: `${gym.name}` })
-  );
 
   return (
     <>
-      {gymData && (
-        <>
-          <h2>Set home gym</h2>
-          <form onSubmit={submitHandler}>
-            <label>Select Gym</label>
-            <Select
-              required
-              value={gym}
-              onChange={setGym}
-              data={selectData}
-            ></Select>
-            <Button type="submit">Set Gym</Button>
-          </form>
-        </>
-      )}
-
+      <DynamicComponent />
       <>
         <h3>Send some feedback</h3>
         <form>
@@ -62,7 +21,7 @@ export default function UserSettingsPage() {
         </form>
       </>
       <>
-        {session?.user?.email === "casey.turgeon@gmail.com" && (
+        {session && session.user?.email === "casey.turgeon@gmail.com" && (
           <>
             <div>
               <p></p>
