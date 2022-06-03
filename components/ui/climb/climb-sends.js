d761c163562088e6437a0766936ae47f0436ecd6
cturgeon <casey.turgeon@gmail.com>
2022-06-02 19:24:03 -0500

    add: change to sends modal

--- components/ui/climb/climb-sends.js
+++ components/ui/climb/climb-sends.js
@@ -9,34 +9,16 @@ export default function ClimbSendModal(props) {
 
   const { id, name, grade, description, image } = props.items;
 
-  function submitHandler(event) {
-    event.preventDefault();
-
-    showNotification({
-      icon: <Check size={18} />,
-      title: `You sent ${name}`,
-      message: "We logged your climb. Keep crushing! ",
-      autoClose: 4000,
-    });
-  }
-
   return (
     <>
       <Modal
         opened={opened}
         onClose={() => setOpened(false)}
-        title="Talk beta and add comments"
+        title="List of crushers!"
       >
         {
           <Box>
-            <form onSubmit={submitHandler}>
-              <Textarea
-                placeholder="Talk beta or add a comment"
-                label="Your Comment"
-                required
-              />
-              <Button type="submit">Add comment</Button>
-            </form>
+            <Text> Name 🧗</Text>
           </Box>
         }
       </Modal>
