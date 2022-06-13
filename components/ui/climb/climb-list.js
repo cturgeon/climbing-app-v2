1da82badba86c1930e54c4798a5e1090a29274dd
cturgeon <casey.turgeon@gmail.com>
2022-06-13 10:38:07 -0500

    add: viewportSize for cards

--- components/ui/climb/climb-list.js
+++ components/ui/climb/climb-list.js
@@ -1,18 +1,30 @@
 import { Grid } from "@mantine/core";
+import { useViewportSize } from "@mantine/hooks";
 
 import ClimbItem from "./climb-item";
 import ClimbCard from "./climb-card";
+import ClimbCardMobile from "./climb-card-mobile";
 
 // props from [wallId]
 export default function ClimbList(props) {
   const { routes, wall } = props.items;
+
+  const { width } = useViewportSize();
 
   return (
     <ul style={{ padding: 0 }}>
       <Grid justify="center" spacing="md">
         {routes.map((route) => (
           <ClimbItem key={route.id}>
-            <ClimbCard items={{ route, wall }} />
+            {width > 1200 ? (
+              <>
+                <ClimbCard items={{ route, wall }} />
+              </>
+            ) : (
+              <>
+                <ClimbCardMobile items={{ route, wall }} />
+              </>
+            )}
           </ClimbItem>
         ))}
       </Grid>
