b8f8ddee1103877240b51abcf00bc16f26bfb769
cturgeon <casey.turgeon@gmail.com>
2022-07-04 13:41:43 -0500

    update: climb-card mantine to next/image

--- components/ui/climb/climb-card.js
+++ components/ui/climb/climb-card.js
@@ -1,13 +1,7 @@
 import { Circles } from "tabler-icons-react";
-import {
-  Card,
-  Image,
-  Text,
-  Group,
-  useMantineTheme,
-  Grid,
-  Box,
-} from "@mantine/core";
+import { Card, Text, Group, useMantineTheme, Grid, Box } from "@mantine/core";
+
+import Image from "next/image";
 
 import ClimbCommentsModal from "./climb-comments-modal";
 import ClimbLogModal from "./climb-log-modal";
@@ -23,10 +17,18 @@ export default function ClimbCard(props) {
     theme.colorScheme === "dark" ? theme.colors.dark[1] : theme.colors.gray[7];
 
   return (
-    <Box sx={{ maxWidth: 300 }} mx="auto">
+    <Box sx={{ minWidth: 300 }} mx="auto" pb={"md"}>
       <Card shadow="sm" p="lg">
         <Card.Section>
-          <Image src={image} alt={id} />
+          <div
+            style={{
+              position: "relative",
+              width: "100%",
+              paddingBottom: "100%",
+            }}
+          >
+            <Image src={image} alt={id} layout="fill" />
+          </div>
         </Card.Section>
         <Group
           position="apart"
