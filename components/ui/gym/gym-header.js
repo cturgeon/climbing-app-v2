8fc9ab97d8d2c2999a8edb3094357efeb606a05a
cturgeon <casey.turgeon@gmail.com>
2022-07-04 12:35:21 -0500

    change: gym-header mantine to next-image

--- components/ui/gym/gym-header.js
+++ components/ui/gym/gym-header.js
@@ -1,12 +1,27 @@
-import { Image, Stack, Text, Divider } from "@mantine/core";
+import { Stack, Text, Divider } from "@mantine/core";
+import Image from "next/image";
 
 export default function GymHeader(props) {
   const { id, name, image, description, address } = props.items;
   return (
     <>
+      <div
+        style={{
+          position: "relative",
+          width: "100%",
+          paddingBottom: "50%",
+        }}
+      >
+        <Image
+          alt={id}
+          radius="md"
+          src={image}
+          layout="fill"
+          objectFit="contain"
+        />
+      </div>
       <div style={{ marginLeft: "auto", marginRight: "auto" }}>
         <Stack align="center">
-          <Image alt={id} radius="md" src={image} />
           <Text>{name}</Text>
           <Text>{address}</Text>
           <Text>{description}</Text>
