842e974825da1de87517277f10e100c7829892b8
cturgeon <casey.turgeon@gmail.com>
2022-07-14 13:47:12 -0500

    update: function change for logs

--- components/log-visuals/bar-chart.js
+++ components/log-visuals/bar-chart.js
@@ -3,7 +3,25 @@ import { BarChart, XYPlot, HorizontalGridLines } from "react-d3-components";
 
 export default function BarChartContainer(props) {
   const climbData = props.items;
-  // go through the data, x axis will be grade, y will be count at each grade
+  const data = dataHandler(climbData);
+  return (
+    <h1 className="title">
+      Climbs
+      <p className="age">
+        <span /> Grade
+      </p>
+      <BarChart
+        data={data}
+        width={347}
+        height={307}
+        margin={{ top: 10, bottom: 50, left: 50, right: 0 }}
+      />
+    </h1>
+  );
+}
+
+// go through the data, x axis will be grade, y will be count at each grade
+function dataHandler(climbData) {
   let mappedData = {
     // to be filled with grade: count
   };
@@ -16,13 +34,9 @@ export default function BarChartContainer(props) {
     // to be filled with x: 'grade', y: count
   ];
 
-  console.log(mappedData);
-
   for (let key in mappedData) {
     values.push({ x: key, y: mappedData[key] });
   }
-
-  console.log(values);
 
   let data = [
     {
@@ -30,18 +44,5 @@ export default function BarChartContainer(props) {
       values: values,
     },
   ];
-  return (
-    <h1 className="title">
-      Climbs
-      <p className="age">
-        <span class="dot" /> Grade
-      </p>
-      <BarChart
-        data={data}
-        width={347}
-        height={307}
-        margin={{ top: 10, bottom: 50, left: 50, right: 0 }}
-      />
-    </h1>
-  );
+  return data;
 }
